0dc55b533f9851f0fa0889119cf29ec831e8d25f
Manuel Freiberger <manuel.freiberger@gmx.at>
2013-09-10 22:44:20 +0200

    Added tests for recursive_timed_mutex

---
+++ keil_cmsis_rtos/test/mutex/tst_recursive_timed_mutex.cpp
@@ -0,0 +1,287 @@
+/*******************************************************************************
+  WEOS - Wrapper for embedded operating systems
+
+  Copyright (c) 2013, Manuel Freiberger
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+  - Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+  - Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+  POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+#include "../../mutex.hpp"
+#include "sparring_recursive_timed.hpp"
+
+#include "gtest/gtest.h"
+
+TEST(recursive_timed_mutex, Constructor)
+{
+    weos::recursive_timed_mutex m;
+}
+
+TEST(recursive_timed_mutex, lock)
+{
+    weos::recursive_timed_mutex m;
+    m.lock();
+    m.lock();
+    m.unlock();
+    m.unlock();
+}
+
+TEST(recursive_timed_mutex, try_lock)
+{
+    weos::recursive_timed_mutex m;
+    bool result;
+    result = m.try_lock();
+    ASSERT_TRUE(result);
+    result = m.try_lock();
+    ASSERT_TRUE(result);
+    m.unlock();
+    m.unlock();
+
+    m.lock();
+    result = m.try_lock();
+    ASSERT_TRUE(result);
+    m.unlock();
+    m.unlock();
+
+    result = m.try_lock();
+    ASSERT_TRUE(result);
+    m.lock();
+    m.unlock();
+    m.unlock();
+}
+
+TEST(recursive_timed_mutex, try_lock_for)
+{
+    weos::recursive_timed_mutex m;
+    bool result;
+    result = m.try_lock_for(weos::chrono::milliseconds(5));
+    ASSERT_TRUE(result);
+    result = m.try_lock_for(weos::chrono::milliseconds(5));
+    ASSERT_TRUE(result);
+    m.unlock();
+    m.unlock();
+
+    m.lock();
+    result = m.try_lock_for(weos::chrono::milliseconds(5));
+    ASSERT_TRUE(result);
+    m.unlock();
+    m.unlock();
+
+    result = m.try_lock_for(weos::chrono::milliseconds(5));
+    ASSERT_TRUE(result);
+    m.lock();
+    m.unlock();
+    m.unlock();
+}
+
+// ----=====================================================================----
+//     Tests together with a sparring thread
+// ----=====================================================================----
+
+TEST(sparring_recursive_timed_mutex, lock)
+{
+    SparringData data;
+    osThreadId sparringId = osThreadCreate(&sparringThread, &data);
+    ASSERT_TRUE(sparringId != 0);
+    osDelay(10);
+    ASSERT_TRUE(data.sparringStarted);
+
+    data.mutex.lock();
+    data.action = SparringData::MutexLock;
+    osDelay(10);
+    ASSERT_TRUE(data.busy);
+    ASSERT_EQ(0, data.numLocks);
+
+    data.mutex.lock();
+    osDelay(10);
+    ASSERT_TRUE(data.busy);
+    ASSERT_EQ(0, data.numLocks);
+
+    data.mutex.unlock();
+    osDelay(10);
+    ASSERT_TRUE(data.busy);
+    ASSERT_EQ(0, data.numLocks);
+
+    data.mutex.unlock();
+    osDelay(10);
+    ASSERT_FALSE(data.busy);
+    ASSERT_EQ(1, data.numLocks);
+
+    ASSERT_FALSE(data.mutex.try_lock());
+
+    data.action = SparringData::MutexLock;
+    osDelay(10);
+    ASSERT_FALSE(data.busy);
+    ASSERT_EQ(2, data.numLocks);
+
+    ASSERT_FALSE(data.mutex.try_lock());
+
+    data.action = SparringData::MutexUnlock;
+    osDelay(10);
+    ASSERT_FALSE(data.busy);
+    ASSERT_EQ(1, data.numLocks);
+
+    ASSERT_FALSE(data.mutex.try_lock());
+
+    data.action = SparringData::MutexUnlock;
+    osDelay(10);
+    ASSERT_FALSE(data.busy);
+    ASSERT_EQ(0, data.numLocks);
+
+    data.action = SparringData::Terminate;
+    osDelay(10);
+}
+
+TEST(sparring_recursive_timed_mutex, try_lock)
+{
+    SparringData data;
+    osThreadId sparringId = osThreadCreate(&sparringThread, &data);
+    ASSERT_TRUE(sparringId != 0);
+    osDelay(10);
+    ASSERT_TRUE(data.sparringStarted);
+
+    data.mutex.lock();
+    data.action = SparringData::MutexTryLock;
+    osDelay(10);
+    ASSERT_FALSE(data.busy);
+    ASSERT_EQ(0, data.numLocks);
+
+    data.mutex.lock();
+    data.action = SparringData::MutexTryLock;
+    osDelay(10);
+    ASSERT_FALSE(data.busy);
+    ASSERT_EQ(0, data.numLocks);
+
+    data.mutex.unlock();
+    data.action = SparringData::MutexTryLock;
+    osDelay(10);
+    ASSERT_FALSE(data.busy);
+    ASSERT_EQ(0, data.numLocks);
+
+    data.mutex.unlock();
+    data.action = SparringData::MutexTryLock;
+    osDelay(10);
+    ASSERT_FALSE(data.busy);
+    ASSERT_EQ(1, data.numLocks);
+
+    ASSERT_FALSE(data.mutex.try_lock());
+
+    data.action = SparringData::MutexTryLock;
+    osDelay(10);
+    ASSERT_FALSE(data.busy);
+    ASSERT_EQ(2, data.numLocks);
+
+    ASSERT_FALSE(data.mutex.try_lock());
+
+    data.action = SparringData::MutexUnlock;
+    osDelay(10);
+    ASSERT_FALSE(data.busy);
+    ASSERT_EQ(1, data.numLocks);
+
+    ASSERT_FALSE(data.mutex.try_lock());
+
+    data.action = SparringData::MutexUnlock;
+    osDelay(10);
+    ASSERT_FALSE(data.busy);
+    ASSERT_EQ(0, data.numLocks);
+
+    data.action = SparringData::Terminate;
+    osDelay(10);
+}
+
+TEST(sparring_recursive_timed_mutex, try_lock_for)
+{
+    SparringData data;
+    osThreadId sparringId = osThreadCreate(&sparringThread, &data);
+    ASSERT_TRUE(sparringId != 0);
+    osDelay(10);
+    ASSERT_TRUE(data.sparringStarted);
+
+    data.mutex.lock();
+    data.action = SparringData::MutexTryLockFor;
+    osDelay(10);
+    ASSERT_FALSE(data.busy);
+    ASSERT_EQ(0, data.numLocks);
+
+    data.mutex.lock();
+    data.action = SparringData::MutexTryLockFor;
+    osDelay(10);
+    ASSERT_FALSE(data.busy);
+    ASSERT_EQ(0, data.numLocks);
+
+    data.mutex.unlock();
+    data.action = SparringData::MutexTryLockFor;
+    osDelay(10);
+    ASSERT_FALSE(data.busy);
+    ASSERT_EQ(0, data.numLocks);
+
+    data.mutex.unlock();
+    data.action = SparringData::MutexTryLockFor;
+    osDelay(10);
+    ASSERT_FALSE(data.busy);
+    ASSERT_EQ(1, data.numLocks);
+
+    ASSERT_FALSE(data.mutex.try_lock());
+    ASSERT_FALSE(data.mutex.try_lock_for(weos::chrono::milliseconds(5)));
+
+    data.action = SparringData::MutexTryLockFor;
+    osDelay(10);
+    ASSERT_FALSE(data.busy);
+    ASSERT_EQ(2, data.numLocks);
+
+    ASSERT_FALSE(data.mutex.try_lock());
+    ASSERT_FALSE(data.mutex.try_lock_for(weos::chrono::milliseconds(5)));
+
+    data.action = SparringData::MutexUnlock;
+    osDelay(10);
+    ASSERT_FALSE(data.busy);
+    ASSERT_EQ(1, data.numLocks);
+
+    ASSERT_FALSE(data.mutex.try_lock());
+    ASSERT_FALSE(data.mutex.try_lock_for(weos::chrono::milliseconds(5)));
+
+    data.action = SparringData::MutexUnlock;
+    osDelay(10);
+    ASSERT_FALSE(data.busy);
+    ASSERT_EQ(0, data.numLocks);
+
+    data.mutex.lock();
+    data.action = SparringData::MutexTryLockFor;
+    osDelay(1);
+    ASSERT_TRUE(data.busy);
+    ASSERT_EQ(0, data.numLocks);
+    data.mutex.unlock();
+    osDelay(10);
+    ASSERT_FALSE(data.busy);
+    ASSERT_EQ(1, data.numLocks);
+
+    ASSERT_FALSE(data.mutex.try_lock());
+    ASSERT_FALSE(data.mutex.try_lock_for(weos::chrono::milliseconds(5)));
+
+    data.action = SparringData::MutexUnlock;
+    osDelay(10);
+    ASSERT_FALSE(data.busy);
+    ASSERT_EQ(0, data.numLocks);
+
+    data.action = SparringData::Terminate;
+    osDelay(10);
+}
