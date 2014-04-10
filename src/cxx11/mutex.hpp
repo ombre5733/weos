b85d1a684bedab1b109815b119d6de502969cf9a
Manuel Freiberger <manuel.freiberger@gmx.at>
2014-04-10 19:45:12 +0200

    Bring defer_lock_t and try_to_lock_t into the weos namespace

--- src/cxx11/mutex.hpp
+++ src/cxx11/mutex.hpp
@@ -44,6 +44,12 @@ using std::unique_lock;
 using std::adopt_lock_t;
 using std::adopt_lock;
 
+using std::defer_lock_t;
+using std::defer_lock;
+
+using std::try_to_lock_t;
+using std::try_to_lock;
+
 } // namespace weos
 
 #endif // WEOS_CXX11_MUTEX_HPP
