9416cecb27664ccd22fe827db17d927ed93cba6a
Manuel Freiberger <manuel.freiberger@gmx.at>
2015-02-24 18:49:46 +0100

    Clean the message_queue implementation.

--- src/keil_cmsis_rtos/messagequeue.hpp
+++ src/keil_cmsis_rtos/messagequeue.hpp
@@ -44,17 +44,17 @@ WEOS_BEGIN_NAMESPACE
 //! A message queue.
 //! The message_queue is an object to pass elements from one thread to another
 //! in a thread-safe manner. The object statically holds the necessary memory.
-template <typename TypeT, std::size_t QueueSizeT>
+template <typename TType, std::size_t TQueueSize>
 class message_queue
 {
     // The CMSIS message queue operates on elements of type uint32_t.
-    static_assert(sizeof(TypeT) <= 4,
+    static_assert(sizeof(TType) <= 4,
                   "Implementation limits element size to 32 bit.");
-    static_assert(QueueSizeT > 0, "The queue size must be non-zero.");
+    static_assert(TQueueSize > 0, "The queue size must be non-zero.");
 
 public:
     //! The type of the elements transfered via this message queue.
-    typedef TypeT element_type;
+    typedef TType element_type;
 
     //! Creates a message queue.
     //! Creates an empty message queue.
@@ -64,7 +64,7 @@ public:
         // Keil's CMSIS RTOS wants a zero'ed control block type for
         // initialization.
         m_queueData[0] = 0;
-        osMessageQDef_t queueDef = { QueueSizeT, m_queueData };
+        osMessageQDef_t queueDef = { TQueueSize, m_queueData };
         m_id = osMessageCreate(&queueDef, NULL);
         if (m_id == 0)
             WEOS_THROW_SYSTEM_ERROR(cmsis_error::osErrorOS,
@@ -75,7 +75,7 @@ public:
     //! Returns the maximum number of elements which the queue can hold.
     std::size_t capacity() const
     {
-        return QueueSizeT;
+        return TQueueSize;
     }
 
     //! Receives an element from the queue.
@@ -116,6 +116,7 @@ public:
         return std::pair<bool, element_type>(true, element);
     }
 
+#if 0
     //! Tries to receive an element from the queue.
     //! Tries to receive an element from the message queue within the timeout
     //! duration \p d.
@@ -137,6 +138,7 @@ public:
 
         return std::pair<bool, element_type>(false, element_type());
     }
+#endif
 
     //! Sends an element via the queue.
     //! Sends the \p element by appending it at the end of the message queue.
@@ -174,6 +176,7 @@ public:
         return false;
     }
 
+#if 0
     //! Tries to send an element via the queue.
     //! Tries to send the given \p element via the queue and returns \p true
     //! if successful. If there is no space available within the
@@ -188,93 +191,13 @@ public:
         return chrono::detail::cmsis_wait<
                 RepT, PeriodT, try_sender>::wait(d, sender);
     }
+#endif
 
 private:
     //! The storage for the message queue.
-    std::uint32_t m_queueData[4 + QueueSizeT];
+    std::uint32_t m_queueData[4 + TQueueSize];
     //! The id of the message queue.
     osMessageQId m_id;
-
-    // A helper to wait for an element in the message queue.
-    struct try_receiver
-    {
-        try_receiver(osMessageQId id)
-            : m_id(id),
-              m_datum(0)
-        {
-        }
-
-        // Returns the datum which has been read from the queue.
-        std::uint32_t& datum()
-        {
-            return m_datum;
-        }
-
-        // Waits up to \p millisec milliseconds for getting an element from
-        // the message queue. Returns \p true, if an element has been acquired
-        // and no further waiting is necessary.
-        bool operator() (std::int32_t millisec)
-        {
-            osEvent result = osMessageGet(m_id, millisec);
-            if (result.status == osEventMessage)
-            {
-                m_datum = result.value.v;
-                return true;
-            }
-
-            if (   result.status != osOK
-                && result.status != osEventTimeout)
-            {
-                WEOS_THROW_SYSTEM_ERROR(
-                            cmsis_error::cmsis_error_t(result.status),
-                            "message_queue::try_receiver failed");
-            }
-
-            return false;
-        }
-
-    private:
-        // A pointer to the message queue from which an element shall be
-        // fetched.
-        osMessageQId m_id;
-        // The element which has been taken from the queue.
-        std::uint32_t m_datum;
-    };
-
-    // A helper to put an element into the message queue.
-    struct try_sender
-    {
-        try_sender(osMessageQId id, std::uint32_t datum)
-            : m_id(id),
-              m_datum(datum)
-        {
-        }
-
-        // Waits up to \p millisec milliseconds for putting an element into
-        // the message queue. Returns \p true, if the element has been enqueued
-        // and no further waiting is necessary.
-        bool operator() (std::int32_t millisec) const
-        {
-            osStatus status = osMessagePut(m_id, m_datum, millisec);
-            if (status == osOK)
-                return true;
-
-            if (   status != osErrorTimeoutResource
-                && status != osErrorResource)
-            {
-                WEOS_THROW_SYSTEM_ERROR(cmsis_error::cmsis_error_t(status),
-                                        "message_queue::try_sender failed");
-            }
-
-            return false;
-        }
-
-    private:
-        // A pointer to the message queue into which the element shall be put.
-        osMessageQId m_id;
-        // The datum to be put into the queue.
-        std::uint32_t m_datum;
-    };
 };
 
 WEOS_END_NAMESPACE
