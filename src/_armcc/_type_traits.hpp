40d46c5717fe53a48e20983597083fb8ceb24db5
Manuel Freiberger <manuel.freiberger@gmx.at>
2016-07-19 22:48:24 +0200

    Added type traits aliases.

--- src/_armcc/_type_traits.hpp
+++ src/_armcc/_type_traits.hpp
@@ -87,10 +87,13 @@ using boost::is_same;
 using boost::is_scalar;
 using boost::is_void;
 using boost::is_volatile;
+using boost::make_signed;
+using boost::make_unsigned;
 using boost::remove_all_extents;
 using boost::remove_const;
 using boost::remove_cv;
 using boost::remove_extent;
+using boost::remove_pointer;
 using boost::remove_reference;
 using boost::remove_volatile;
 using boost::result_of;
@@ -194,6 +197,85 @@ struct is_union : public integral_constant<bool, __is_union(TType)> {};
 
 template <typename TType>
 struct is_trivially_copyable : public integral_constant<bool, __is_trivially_copyable(TType)> {};
+
+// ----=----
+// 20.13.7.1, const-volatile modifications:
+template <class T>
+using remove_const_t = typename remove_const<T>::type;
+
+template <class T>
+using remove_volatile_t = typename remove_volatile<T>::type;
+
+template <class T>
+using remove_cv_t = typename remove_cv<T>::type;
+
+template <class T>
+using add_const_t = typename add_const<T>::type;
+
+template <class T>
+using add_volatile_t = typename add_volatile<T>::type;
+
+template <class T>
+using add_cv_t = typename add_cv<T>::type;
+
+// 20.13.7.2, reference modifications:
+template <class T>
+using remove_reference_t = typename remove_reference<T>::type;
+
+template <class T>
+using add_lvalue_reference_t = typename add_lvalue_reference<T>::type;
+
+template <class T>
+using add_rvalue_reference_t = typename add_rvalue_reference<T>::type;
+
+// 20.13.7.3, sign modifications:
+template <class T>
+using make_signed_t = typename make_signed<T>::type;
+
+template <class T>
+using make_unsigned_t = typename make_unsigned<T>::type;
+
+// 20.13.7.4, array modifications:
+template <class T>
+using remove_extent_t = typename remove_extent<T>::type;
+
+template <class T>
+using remove_all_extents_t = typename remove_all_extents<T>::type;
+
+// 20.13.7.5, pointer modifications:
+template <class T>
+using remove_pointer_t = typename remove_pointer<T>::type;
+
+template <class T>
+using add_pointer_t = typename add_pointer<T>::type;
+
+// 20.13.7.6, other transformations:
+template <std::size_t Len, std::size_t Align = std::size_t(-1)> 
+using aligned_storage_t = typename aligned_storage<Len, Align>::type;
+
+//template <std::size_t Len, class... Types>
+//using aligned_union_t = typename aligned_union<Len, Types...>::type;
+
+template <class T>
+using decay_t = typename decay<T>::type;
+
+template <bool b, class T = void>
+using enable_if_t = typename enable_if<b, T>::type;
+
+template <bool b, class T, class F>
+using conditional_t = typename conditional<b, T, F>::type;
+
+template <class... T>
+using common_type_t = typename common_type<T...>::type;
+
+//template <class T>
+//using underlying_type_t = typename underlying_type<T>::type;
+
+template <class T>
+using result_of_t = typename result_of<T>::type;
+
+template <class...>
+using void_t = void;
 
 } // namespace std
 
